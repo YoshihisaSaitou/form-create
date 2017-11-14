325a5126366ce519cceacde445b181e47e24daf8
Saitou Yoshihisa <anti_viral_drug@yahoo.co.jp>
2017-11-15 08:00:41 +0900

    edit

--- FormCreate.php
+++ FormCreate.php
@@ -1,34 +1,35 @@
 <?php
-/**
- * @package FormCreate
- * @version 1.0
- */
 /*
- Plugin Name: FormCreate
- Plugin URI: https://github.com/YoshihisaSaitou/form-create
- Description: フォーム作成プラグイン
- Version: 1.0
- Author: Yoshihisa Saito
- Author URI: https://github.com/YoshihisaSaitou/form-create
- License: GPL2
- Text Domain: form-create
- Domain Path: /languages/
- */
+@package FormCreate
+@version 1.0
+
+Plugin Name: FormCreate
+Plugin URI: https://github.com/YoshihisaSaitou/form-create
+Description: フォーム作成プラグイン
+Version: 1.0
+Author: Yoshihisa Saito
+Author URI: https://github.com/YoshihisaSaitou/form-create
+License: GPL2
+Text Domain: form-create
+Domain Path: /languages/
+*/
 
 namespace FormCreate;
 
-require_once('config/App.php');
+//require_once('config/App.php');
 require_once('config/Utility.php');
 require_once('config/Environment.php');
-
 require_once('controllers/BaseController.php');
-require_once('controllers/admin/AdminController.php');
-require_once('controllers/admin/TopController.php');
-require_once('controllers/admin/CreateController.php');
-require_once('controllers/admin/DeleteController.php');
-require_once('controllers/admin/EditController.php');
-require_once('controllers/admin/ListController.php');
 
+//管理画面を表示している場合のみ実行
+//if(is_admin()){
+//require_once('controllers/admin/AdminController.php');
+//require_once('controllers/admin/TopController.php');
+//require_once('controllers/admin/CreateController.php');
+//require_once('controllers/admin/DeleteController.php');
+//require_once('controllers/admin/EditController.php');
+//require_once('controllers/admin/ListController.php');
+//}
 use FormCreate\config\Environment;
 use FormCreate\controllers\admin\AdminController;
 use FormCreate\controllers\admin\TopController;
@@ -38,6 +39,6 @@ use FormCreate\controllers\admin\EditController;
 use FormCreate\controllers\admin\ListController;
 
 // 管理画面を表示している場合のみ実行します。
-if( is_admin() ) {
-    $form_create_page = new TopController();
-}
+//if( is_admin() ) {
+//    $form_create_page = new TopController();
+//}
